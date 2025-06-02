e65cd409bda47c95d9403ee6d673b8121b15169d
nesaratai <nesaratai@gmail.com>
2025-06-02 18:03:40 -0400

    exercise 3 and 4 make the path for each of links and make a new state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar/NavBar';
+import MailboxDetails from './components/MailboxDetails/MailboxDetails';
+import MailboxForm from './components/MailboxForm/MailboxForm';
+import MailboxList from './components/MailboxList/MailboxList';
 import './App.css'
 
 const App = () => {
+  const [mailboxes, setMailboxes] = useState([]);
+
   return (
   <>
    <NavBar />
-   <p>Hello World</p>
+   <Routes>
+        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+        <Route path="/mailboxes" element={<MailboxList />} />
+        <Route path="/new-mailbox" element={<MailboxForm />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
+      </Routes>
   </>
   )
 };
